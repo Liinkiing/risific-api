af4ea7a29393ec5f14878bbf0a7daec99edce843
Omar Jbara <omar.Jbara2@gmail.com>
2018-07-26 00:01:56 +0200

    - Changed command name

--- src/Command/AppImportRisificCommand.php
+++ src/Command/AppImportRisificCommand.php
@@ -10,9 +10,9 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class AppExportRisificCommand extends Command
+class AppImportRisificCommand extends Command
 {
-    protected static $defaultName = 'app:export-risific';
+    protected static $defaultName = 'app:import-risific';
     protected $importer;
     protected $projectDir;
     protected $filesystem;
